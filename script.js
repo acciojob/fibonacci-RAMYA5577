368dd6e538e560955ba7b6cdea3e34d8c0e0947d
RAMYA5577 <121097897+RAMYA5577@users.noreply.github.com>
2023-12-27 18:26:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	int num1 = 0;
+	int num2 = 1;
+	int ans = 0;
+	for(let i=0;i<num;i++){
+	ans = num1+num2;
+		num1 = num2;
+		num2 = ans;
+	}
+	return ans;
 }
 
 module.exports = fibonacci;
