e25400d1c8f8597e1facbc10436802896a671521
RAMYA5577 <121097897+RAMYA5577@users.noreply.github.com>
2024-07-23 16:42:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function fibonacci(num) {
 // your code here
-	if (num === 0) return 0;
-    if (num === 1) return 1;
+	if (num === 1) return 0;
+    if (num === 2) return 1;
 	let num1 = 0;
 	let num2 = 1;
 	let ans = 0;
-	for(let i=2;i<=num;i++){
+	for(let i=2;i<num;i++){
 	ans = num1+num2;
 		num1 = num2;
 		num2 = ans;
